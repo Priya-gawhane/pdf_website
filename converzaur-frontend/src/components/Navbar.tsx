bfb73964156eb3f82576a3db5195fc31fcc032e4
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-19 14:38:59 +0530

    Navbar completed

---
+++ converzaur-frontend/src/components/Navbar.tsx
@@ -0,0 +1,118 @@
+import {
+    Menubar,
+    MenubarCheckboxItem,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarRadioGroup,
+    MenubarRadioItem,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger,
+  } from "@/components/ui/menubar";
+  
+  export function MenubarDemo() {
+    return (
+      <div className="h-20 flex px-7 md:px-10 items-center justify-center bg-purple-900">
+        <div className="flex justify-between w-full items-center text-white">
+          <h1 className="md:text-4xl text-2xl font-bold">Converzaur</h1>
+          <Menubar className="w-[500px] md:text-xl font-semibold text-sm flex justify-evenly items-center">
+            <MenubarMenu>
+              <MenubarTrigger>Home</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  New Tab
+                </MenubarItem>
+                <MenubarItem>
+                  New Window
+                </MenubarItem>
+                <MenubarItem disabled>New Incognito Window</MenubarItem>
+                <MenubarSeparator />
+                
+                <MenubarSeparator />
+                <MenubarItem>
+                  Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Convert PDF</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  Undo
+                </MenubarItem>
+                <MenubarItem>
+                  Redo
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarSub>
+                  <MenubarSubTrigger>Find</MenubarSubTrigger>
+                  <MenubarSubContent>
+                    <MenubarItem>Search the web</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Find...</MenubarItem>
+                    <MenubarItem>Find Next</MenubarItem>
+                    <MenubarItem>Find Previous</MenubarItem>
+                  </MenubarSubContent>
+                </MenubarSub>
+                <MenubarSeparator />
+                <MenubarItem>Cut</MenubarItem>
+                <MenubarItem>Copy</MenubarItem>
+                <MenubarItem>Paste</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Audio</MenubarTrigger>
+              <MenubarContent>
+                <MenubarCheckboxItem>
+                  Always Show Bookmarks Bar
+                </MenubarCheckboxItem>
+                <MenubarCheckboxItem checked>
+                  Always Show Full URLs
+                </MenubarCheckboxItem>
+                <MenubarSeparator />
+                <MenubarItem inset>
+                  Reload <MenubarShortcut>⌘R</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem disabled inset>
+                  Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem inset>Hide Sidebar</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Image</MenubarTrigger>
+              <MenubarContent>
+                <MenubarRadioGroup value="benoit">
+                  <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+                  <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+                  <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+                </MenubarRadioGroup>
+                <MenubarSeparator />
+                <MenubarItem inset>Edit...</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem inset>Add Profile...</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </div>
+      </div>
+    );
+  }
+  
+  const Navbar = () => {
+    return (
+      <div className="w-full h-20">
+        <MenubarDemo />
+      </div>
+    );
+  };
+  
+  export default Navbar;
+  
